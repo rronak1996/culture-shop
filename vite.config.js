cb8170d2ab7c85bd20b520c6500461ca76b07d55
rronak1996 <rronak1996@users.noreply.github.com>
2025-12-08 22:53:05 +0530

    Fix: Correct base path to culture-shop

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/culture-choco/',
+    base: '/culture-shop/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
